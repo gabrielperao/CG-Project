c8cbca8850c51eff2ef615e6f1b2d897d3d96f3c
gabrielperao <gabrielperao@hotmail.com>
2024-05-14 20:50:45 -0300

    Add extra layer of dirt to base chunk

--- src/chunk/builder/garden_builder.py
+++ src/chunk/builder/garden_builder.py
@@ -4,17 +4,17 @@ from src.chunk import Chunk
 
 class GardenBuilder:
     @staticmethod
-    def build(chunk: Chunk) -> None:
-        chunk.put_object((10, 1, 7), ObjectId.FLOWER)
-        chunk.put_object((8, 1, 5), ObjectId.FLOWER)
-        chunk.put_object((3, 1, 10), ObjectId.FLOWER)
-        chunk.put_object((0, 1, 0), ObjectId.FLOWER)
-        chunk.put_object((2, 1, 12), ObjectId.FLOWER)
-        chunk.put_object((9, 1, 8), ObjectId.FLOWER)
-        chunk.put_object((11, 1, 3), ObjectId.FLOWER)
-        chunk.put_object((13, 1, 6), ObjectId.FLOWER)
-        chunk.put_object((5, 1, 11), ObjectId.FLOWER)
-        chunk.put_object((3, 1, 15), ObjectId.FLOWER)
-        chunk.put_object((6, 1, 14), ObjectId.FLOWER)
-        chunk.put_object((11, 1, 10), ObjectId.TORCH)
-        chunk.put_object((0, 1, 15), ObjectId.TORCH)
+    def build(chunk: Chunk, x, y, z) -> None:
+        chunk.put_object((x + 10, y + 1, z + 7), ObjectId.FLOWER)
+        chunk.put_object((x + 8, y + 1, z + 5), ObjectId.FLOWER)
+        chunk.put_object((x + 3, y + 1, z + 10), ObjectId.FLOWER)
+        chunk.put_object((x + 0, y + 1, z + 0), ObjectId.FLOWER)
+        chunk.put_object((x + 2, y + 1, z + 12), ObjectId.FLOWER)
+        chunk.put_object((x + 9, y + 1, z + 8), ObjectId.FLOWER)
+        chunk.put_object((x + 11, y + 1, z + 3), ObjectId.FLOWER)
+        chunk.put_object((x + 13, y + 1, z + 6), ObjectId.FLOWER)
+        chunk.put_object((x + 5, y + 1, z + 11), ObjectId.FLOWER)
+        chunk.put_object((x + 3, y + 1, z + 15), ObjectId.FLOWER)
+        chunk.put_object((x + 6, y + 1, z + 14), ObjectId.FLOWER)
+        chunk.put_object((x + 11, y + 1, z + 10), ObjectId.TORCH)
+        chunk.put_object((x + 0, y + 1, z + 15), ObjectId.TORCH)
--- src/manager/chunk_manager.py
+++ src/manager/chunk_manager.py
@@ -10,14 +10,15 @@ class ChunkManager:
         chunk = Chunk(index_x, index_z, max_gpu_data_array_index)
         for i in range(16):
             for j in range(16):
-                chunk.put_object((i, 0, j), ObjectId.GRASS)
+                chunk.put_object((i, 0, j), ObjectId.DIRT)
+                chunk.put_object((i, 1, j), ObjectId.GRASS)
         return chunk
 
     @classmethod
     def generate_chunk(cls, index_x, index_z, max_gpu_data_array_index) -> Chunk:
         base_chunk = cls.__generate_base_chunk(index_x, index_z, max_gpu_data_array_index)
-        HouseBuilder.build(base_chunk, 1, 1, 1)
-        TreeBuilder.build(base_chunk, 10, 1, 10)
-        GardenBuilder.build(base_chunk)
-        base_chunk.put_object((1, 1, 11), ObjectId.SLIME)
+        HouseBuilder.build(base_chunk, 1, 2, 1)
+        TreeBuilder.build(base_chunk, 10, 2, 10)
+        GardenBuilder.build(base_chunk, 0, 1, 0)
+        base_chunk.put_object((1, 2, 11), ObjectId.SLIME)
         return base_chunk
